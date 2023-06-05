c95d705075f6fc8444d16fa6eb768b5630387033
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-05 15:46:13 -0400

    created routes

---
+++ routes/UserRouter.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const controller = require('../controllers/UserController')
+const middleware = require('../middleware')
+
+router.get('/', controller.getAllUsers)
+router.get(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.getUserById
+)
+router.put(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.updateUser
+)
+router.delete(
+  '/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.deleteUser
+)
+
+module.exports = router
