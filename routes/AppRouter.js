6b923943824a7b9308e0b04416580ba892b28d51
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-06 15:42:36 -0400

    added EventRouter to AppRouter

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,8 +1,10 @@
 const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const AuthRouter = require('./AuthRouter')
+const EventRouter = require('./EventRouter')
 
 Router.use('/auth', AuthRouter)
 Router.use('/users', UserRouter)
+Router.use('/event', EventRouter)
 
 module.exports = Router
