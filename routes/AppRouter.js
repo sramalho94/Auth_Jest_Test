1f0b10a1cf89ea651bfba6aecec11456a1ffb0ff
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-08 12:39:09 -0400

    incorporated ticket router

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -2,9 +2,11 @@ const Router = require('express').Router()
 const UserRouter = require('./UserRouter')
 const AuthRouter = require('./AuthRouter')
 const EventRouter = require('./EventRouter')
+const TicketRouter = require('./TicketRouter')
 
 Router.use('/auth', AuthRouter)
 Router.use('/users', UserRouter)
 Router.use('/event', EventRouter)
+Router.use('/ticket', TicketRouter)
 
 module.exports = Router
