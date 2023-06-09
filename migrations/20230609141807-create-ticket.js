82b96aae18b6ffd067fec7249a1c8264eff51630
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-09 12:39:40 -0400

    migration for ticket model

---
+++ migrations/20230609141807-create-ticket.js
@@ -0,0 +1,43 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('tickets', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      eventId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'events',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('tickets')
+  }
+}
